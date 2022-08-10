c00822038af9a733917c3347e3efc4eb7bda6049
pavel.nazarov <pavel.nazarov@axamit.com>
2022-08-10 14:47:14 +0300

    Added logic

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import java.awt.*;
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) throws AWTException {
+        Robot hal = new Robot();
+        Random random = new Random();
+        while(true){
+            hal.delay(1000 * 60);
+            int x = random.nextInt() % 640;
+            int y = random.nextInt() % 480;
+            hal.mouseMove(x,y);
+        }
     }
 }
